8140caac7410a533d625f275000557b32dd890f7
nkkll <152094020+nkkll@users.noreply.github.com>
2025-06-28 01:13:06 +0800

    Update TradeManager.java

--- TradeManager.java
+++ TradeManager.java
@@ -10,7 +10,13 @@ public class TradeManager {
             System.out.println("The difference in value is greater than 1.00$.");
             System.out.println("Continue with trade? [Yes/No]");
             String choice = sc.nextLine();
-        }
-    }
 
+            if(choice.equalsIgnoreCase("Yes")) {
+                return true;      // returns true to indicate the trade was successful and the card will remain in collection.
+            } else if(choice.equalsIgnoreCase("No")) {
+                return false;     // returns false; this will trigger removing the card
+            }
+        }
+        return true;
+    }
 }
