1edaae646b3a81760d21980c9002b80802045d8d
nkkll <152094020+nkkll@users.noreply.github.com>
2025-06-28 01:16:13 +0800

    Update BinderManager.java

--- BinderManager.java
+++ BinderManager.java
@@ -109,7 +109,24 @@ public class BinderManager {
 
                 //declare collection, add incoming card to collection first then pass the cards to trademanager
                 //trade manager
+                trade(outgoingCard, incomingCard);
         }
+    }
+
+   
+    public boolean trade(Card outgoingCard, Card incomingCard) {
+        if(outgoingCard.getActualValue() - incomingCard.getActualValue() >= 1.0) {
+            System.out.println("The difference in value is greater than 1.00$.");
+            System.out.println("Continue with trade? [Yes/No]");
+            String choice = sc.nextLine();
+
+            if(choice.equalsIgnoreCase("Yes")) {
+                return true;      // returns true to indicate the trade was successful and the card will remain in collection.
+            } else if(choice.equalsIgnoreCase("No")) {
+                return false;     // returns false; this will trigger removing the card
+            }
+        }
+        return true;
     }
 
 
